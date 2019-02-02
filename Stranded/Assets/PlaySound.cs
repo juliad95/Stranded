e519a8990fc2682aefc08fd94043c3e47164da55
juliad95 <47272661+juliad95@users.noreply.github.com>
2019-02-02 13:01:32 +0100

    Inital Commit

---
+++ Stranded/Assets/PlaySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Audioclip muss an AudioSource angehängt sein.
+public class PlaySound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        audioSource.Stop();
+    }
+}
